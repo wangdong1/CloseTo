181bcce331ce73791407dc71d2cc23238f6c73b1
wangdong <393961340@qq.com>
2018-01-13 02:47:48 +0800

    首次提交

---
+++ app/src/main/java/lechinn/com/closeto/MainActivity.java
@@ -0,0 +1,36 @@
+package lechinn.com.closeto;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView text;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        text = (TextView) findViewById(R.id.text);
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+
+                break;
+        }
+    }
+}
